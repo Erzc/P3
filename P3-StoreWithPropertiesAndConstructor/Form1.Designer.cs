01b3f795b6bda933075d543c3c1aaee8f647ef65
EricZeh <zehbehdee@gmail.com>
2021-06-30 19:48:41 -0600

    Add project files.

---
+++ P3-StoreWithPropertiesAndConstructor/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace P3_StoreWithPropertiesAndConstructor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.orderNumberLabel = new System.Windows.Forms.Label();
+            this.orderNumberTextBox = new System.Windows.Forms.TextBox();
+            this.numberOfItemsLabel = new System.Windows.Forms.Label();
+            this.numberOfItemsTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.usbRadioButton = new System.Windows.Forms.RadioButton();
+            this.mugRadioButton = new System.Windows.Forms.RadioButton();
+            this.penRadioButton = new System.Windows.Forms.RadioButton();
+            this.textToEngraveLabel = new System.Windows.Forms.Label();
+            this.textToEngraveTextBox = new System.Windows.Forms.TextBox();
+            this.logoCheckBox = new System.Windows.Forms.CheckBox();
+            this.numberOfColorsLabel = new System.Windows.Forms.Label();
+            this.numberOfColorsTextBox = new System.Windows.Forms.TextBox();
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // orderNumberLabel
+            // 
+            this.orderNumberLabel.AutoSize = true;
+            this.orderNumberLabel.Location = new System.Drawing.Point(25, 25);
+            this.orderNumberLabel.Name = "orderNumberLabel";
+            this.orderNumberLabel.Size = new System.Drawing.Size(73, 13);
+            this.orderNumberLabel.TabIndex = 0;
+            this.orderNumberLabel.Text = "Order Number";
+            // 
+            // orderNumberTextBox
+            // 
+            this.orderNumberTextBox.Location = new System.Drawing.Point(118, 22);
+            this.orderNumberTextBox.Name = "orderNumberTextBox";
+            this.orderNumberTextBox.Size = new System.Drawing.Size(150, 20);
+            this.orderNumberTextBox.TabIndex = 1;
+            // 
+            // numberOfItemsLabel
+            // 
+            this.numberOfItemsLabel.AutoSize = true;
+            this.numberOfItemsLabel.Location = new System.Drawing.Point(25, 58);
+            this.numberOfItemsLabel.Name = "numberOfItemsLabel";
+            this.numberOfItemsLabel.Size = new System.Drawing.Size(83, 13);
+            this.numberOfItemsLabel.TabIndex = 2;
+            this.numberOfItemsLabel.Text = "Number of items";
+            // 
+            // numberOfItemsTextBox
+            // 
+            this.numberOfItemsTextBox.Location = new System.Drawing.Point(118, 58);
+            this.numberOfItemsTextBox.Name = "numberOfItemsTextBox";
+            this.numberOfItemsTextBox.Size = new System.Drawing.Size(150, 20);
+            this.numberOfItemsTextBox.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.penRadioButton);
+            this.groupBox1.Controls.Add(this.mugRadioButton);
+            this.groupBox1.Controls.Add(this.usbRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(118, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(150, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Item Type";
+            // 
+            // usbRadioButton
+            // 
+            this.usbRadioButton.AutoSize = true;
+            this.usbRadioButton.Location = new System.Drawing.Point(7, 20);
+            this.usbRadioButton.Name = "usbRadioButton";
+            this.usbRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.usbRadioButton.TabIndex = 0;
+            this.usbRadioButton.TabStop = true;
+            this.usbRadioButton.Text = "USB";
+            this.usbRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // mugRadioButton
+            // 
+            this.mugRadioButton.AutoSize = true;
+            this.mugRadioButton.Location = new System.Drawing.Point(7, 43);
+            this.mugRadioButton.Name = "mugRadioButton";
+            this.mugRadioButton.Size = new System.Drawing.Size(46, 17);
+            this.mugRadioButton.TabIndex = 1;
+            this.mugRadioButton.TabStop = true;
+            this.mugRadioButton.Text = "Mug";
+            this.mugRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // penRadioButton
+            // 
+            this.penRadioButton.AutoSize = true;
+            this.penRadioButton.Location = new System.Drawing.Point(7, 66);
+            this.penRadioButton.Name = "penRadioButton";
+            this.penRadioButton.Size = new System.Drawing.Size(44, 17);
+            this.penRadioButton.TabIndex = 2;
+            this.penRadioButton.TabStop = true;
+            this.penRadioButton.Text = "Pen";
+            this.penRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // textToEngraveLabel
+            // 
+            this.textToEngraveLabel.AutoSize = true;
+            this.textToEngraveLabel.Location = new System.Drawing.Point(28, 208);
+            this.textToEngraveLabel.Name = "textToEngraveLabel";
+            this.textToEngraveLabel.Size = new System.Drawing.Size(110, 13);
+            this.textToEngraveLabel.TabIndex = 5;
+            this.textToEngraveLabel.Text = "Text to engrave/print:";
+            // 
+            // textToEngraveTextBox
+            // 
+            this.textToEngraveTextBox.Location = new System.Drawing.Point(31, 224);
+            this.textToEngraveTextBox.Multiline = true;
+            this.textToEngraveTextBox.Name = "textToEngraveTextBox";
+            this.textToEngraveTextBox.Size = new System.Drawing.Size(237, 43);
+            this.textToEngraveTextBox.TabIndex = 6;
+            // 
+            // logoCheckBox
+            // 
+            this.logoCheckBox.AutoSize = true;
+            this.logoCheckBox.Location = new System.Drawing.Point(118, 274);
+            this.logoCheckBox.Name = "logoCheckBox";
+            this.logoCheckBox.Size = new System.Drawing.Size(56, 17);
+            this.logoCheckBox.TabIndex = 7;
+            this.logoCheckBox.Text = "Logo?";
+            this.logoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // numberOfColorsLabel
+            // 
+            this.numberOfColorsLabel.AutoSize = true;
+            this.numberOfColorsLabel.Location = new System.Drawing.Point(28, 306);
+            this.numberOfColorsLabel.Name = "numberOfColorsLabel";
+            this.numberOfColorsLabel.Size = new System.Drawing.Size(87, 13);
+            this.numberOfColorsLabel.TabIndex = 8;
+            this.numberOfColorsLabel.Text = "Number of colors";
+            // 
+            // numberOfColorsTextBox
+            // 
+            this.numberOfColorsTextBox.Location = new System.Drawing.Point(118, 303);
+            this.numberOfColorsTextBox.Name = "numberOfColorsTextBox";
+            this.numberOfColorsTextBox.Size = new System.Drawing.Size(150, 20);
+            this.numberOfColorsTextBox.TabIndex = 9;
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(31, 379);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ReadOnly = true;
+            this.resultsTextBox.Size = new System.Drawing.Size(237, 76);
+            this.resultsTextBox.TabIndex = 10;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(65, 346);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(62, 23);
+            this.submitButton.TabIndex = 11;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(161, 346);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(62, 23);
+            this.clearButton.TabIndex = 12;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 487);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.numberOfColorsTextBox);
+            this.Controls.Add(this.numberOfColorsLabel);
+            this.Controls.Add(this.logoCheckBox);
+            this.Controls.Add(this.textToEngraveTextBox);
+            this.Controls.Add(this.textToEngraveLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numberOfItemsTextBox);
+            this.Controls.Add(this.numberOfItemsLabel);
+            this.Controls.Add(this.orderNumberTextBox);
+            this.Controls.Add(this.orderNumberLabel);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label orderNumberLabel;
+        private System.Windows.Forms.TextBox orderNumberTextBox;
+        private System.Windows.Forms.Label numberOfItemsLabel;
+        private System.Windows.Forms.TextBox numberOfItemsTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton penRadioButton;
+        private System.Windows.Forms.RadioButton mugRadioButton;
+        private System.Windows.Forms.RadioButton usbRadioButton;
+        private System.Windows.Forms.Label textToEngraveLabel;
+        private System.Windows.Forms.TextBox textToEngraveTextBox;
+        private System.Windows.Forms.CheckBox logoCheckBox;
+        private System.Windows.Forms.Label numberOfColorsLabel;
+        private System.Windows.Forms.TextBox numberOfColorsTextBox;
+        private System.Windows.Forms.TextBox resultsTextBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ P3-StoreWithPropertiesAndConstructor/LogoOrderItem.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_StoreWithPropertiesAndConstructor
+{
+    class LogoOrderItem
+    {
+        //properties with backing fields:
+
+        public bool HasLogo;
+        public string ItemType;
+        public int NumColors;
+        public int NumItems;
+        public string Text;
+
+
+        //auto-property:
+        public int ItemID;
+        //A read only decimal for the total price of all items with all options
+
+
+        //Default values for chaining should be:
+
+        //1 A constructor that takes six parameters (one for each settable property in the class).
+        //In the body of this constructor set each parameter into the appropriate class property or field.
+
+        //2 A constructor that takes 2 parameters: a string for text and a bool for has logo. Chain this constructor to the six parameter constructor.
+
+        //3 A constructor that takes no parameters (a parameterless constructor). Chain this constructor to the six parameter constructor. 
+
+
+        //this calculates the price of the logo item based on the type of item whether or not it is a graphic or text only logo, number of colors and number of items.
+        //Make sure to read the customers’ requirements above carefully and implement their business rules into the Calc() correctly.
+        private void Calc()
+        {
+        }
+
+
+
+        public string GetOrderSummary()
+        {
+            //Use the member variables to return a nicely formatted string like:
+            //Order num 1: 20 mugs with 3 color logo with the following text: C# is a great language! Price: $74.80
+
+
+            //return stuff
+        }
+
+
+
+        //DO THIS: Submit Button will do the following:
+        //1. Instantiate a LogoOrderItem using the two parameter constructor.
+                    //Use the Text to Engrave TextBox.Text and Logo Checkbox.Checked for the two arguments needed for the constructor.
+        //2.	Read the values of the remaining items on the form and set the appropriate properties in your LogoOrderItem.
+        //3.	If there is no logo, do not charge the customer for any colors.
+                    //This could be handled a number of ways, either in Calc() method or by making sure the Number of colors is
+                    //set to 0 if Logo is not checked.
+        //4.	Once all properties in the LogoOrderItem are set, set the Results TextBox.Text to the return value of calling GetOrderSummary().
+        //5.	Wrap the submit button code in a try/catch block.
+                    //Catch the Exception class.
+                    //Display the error message to the results text box if the user fails to input correct values.
+
+
+
+        //A Clear Button resets all the items on the form to the defaults.
+
+
+
+        //DO THIS: Set label and textbox for number of colors to only be visible when Logo is checked.
+        //To do this, handle the CheckBox.CheckedChanged event to set the Visibility properties for the Label and Checkbox.
+}
+}
